6d17065bdcb1a9d42b68c9cf096e54daf0429469
Natalia Shipova <nataliefrance@mail.ru>
2019-07-17 18:08:54 +0300

    Geekbrains_Алгоритмы и структуры данных: сортировки выбором, вставками, пузырьком.

---
+++ src/main/java/GeekBrains_Algorithms/Lesson2/Main.java
@@ -0,0 +1,28 @@
+package GeekBrains_Algorithms.Lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList<Integer> list_1 = new MyArrayList<Integer>();
+        MyArrayList<Integer> list_2 = new MyArrayList<Integer>();
+        MyArrayList<Integer> list_3 = new MyArrayList<Integer>();
+
+        for (int i = 0; i < 100000; i++) {
+            list_1.add((int) (Math.random()*10));
+            list_2.add((int) (Math.random()*10));
+            list_3.add((int) (Math.random()*10));
+        }
+
+        long startSelectionSort = System.currentTimeMillis();
+        list_1.selectionSort();
+        System.out.println("Время сортировки выбором: " + (System.currentTimeMillis() - startSelectionSort) + " милисекунд.");
+
+        long startInsertionSort = System.currentTimeMillis();
+        list_2.insertionSort();
+        System.out.println("Время сортировки вставками: " + (System.currentTimeMillis() - startInsertionSort) + " милисекунд.");
+
+        long startBubbleSort = System.currentTimeMillis();
+        list_3.bubbleSort();
+        System.out.println("Время сортировки пузырьком: " + (System.currentTimeMillis() - startBubbleSort) + " милисекунд.");
+
+    }
+}
---
+++ src/main/java/GeekBrains_Algorithms/Lesson2/MyArrayList.java
@@ -0,0 +1,123 @@
+package GeekBrains_Algorithms.Lesson2;
+
+import java.util.Arrays;
+
+public class MyArrayList<T extends Comparable> {
+    private T[] array;
+    private int size;
+    private final int DEFAULT_CAPACITY = 1000000;
+
+    public MyArrayList(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("wrong capacity: " + capacity);
+        } else {
+            array = (T[]) new Comparable[capacity];
+            size = 0;
+        }
+    }
+
+    MyArrayList() {
+        array = (T[]) new Comparable[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < size; i++) {
+            s += array[i] + " ";
+        }
+        return s;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    //Написать методы удаления, добавления, поиска элемента массива.
+    void add(T item) {
+        array[size] = item;
+        size++;
+    }
+
+    void add(int index, T item) {
+        if (index > size) {
+            size = index;
+        }
+        if (index < 0) {
+            throw new IllegalArgumentException("wrong index: " + index);
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = item;
+        size++;
+    }
+
+    boolean remove(T item) {
+        for (int i = 0; i < size; i++) {
+            if (item.equals(array[i])) {
+                for (int j = i; j < size - 1; j++) {
+                    array[j] = array[j + 1];
+                }
+                size--;
+                array[size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean find(T item){
+        for (int i = 0; i < size; i++) {
+            if (item.equals(array[i])) {
+               return true;
+            }
+        }
+        return false;
+    }
+
+   private void swap (int index1, int index2){
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+
+   }
+
+   void selectionSort(){
+       int minIndex;
+       for (int i = 0; i < size - 1; i++) {
+           minIndex = i;
+           for (int j = i + 1; j < size; j++) {
+               if (array[j].compareTo(array[minIndex]) < 0){
+                  minIndex = j;
+               }
+           }
+           swap(i, minIndex);
+       }
+   }
+
+    void insertionSort(){
+        T key;
+        for (int i = 1; i < size ; i++) {
+            int j = i;
+            key = array[i];
+            while (j > 0 && key.compareTo(array[j - 1]) < 0){
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = key;
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = size - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j + 1].compareTo(array[j]) < 0) {
+                    swap(j + 1, j);
+                }
+            }
+        }
+    }
+}
+
