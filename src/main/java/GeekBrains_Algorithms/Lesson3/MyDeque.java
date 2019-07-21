8aa5aa710ce6524f537521649e4832662c7cff05
Natalia Shipova <nataliefrance@mail.ru>
2019-07-21 17:04:36 +0300

    реализация MyDeque (дек)

---
+++ src/main/java/GeekBrains_Algorithms/Lesson3/MyDeque.java
@@ -0,0 +1,112 @@
+package GeekBrains_Algorithms.Lesson3;
+
+public class MyDeque<T> {
+    private T[] list;
+    private int size;
+    private int begin;
+    private int end;
+    private final int DEFAULT_CAPACITY = 100;
+
+    public MyDeque(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("wrong capacity " + capacity);
+        } else {
+            list = (T[]) new Object[capacity];
+            size = 0;
+            begin = 0;
+            end = 0;
+        }
+    }
+
+    public MyDeque() {
+        this.list = (T[]) new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+        this.begin = 0;
+        this.end = 0;
+    }
+
+    public void insertLeft(T item){
+        if (isFull()) {
+            reCapacity(list.length * 2);
+        }
+        size++;
+        begin = prevIndex(begin);
+        list[begin] = item;
+    }
+
+    public void insertRight(T item){
+        if (isFull()) {
+            reCapacity(list.length * 2);
+        }
+        size++;
+        list[end] = item;
+        end = nextIndex(end);
+    }
+
+    public T removeLeft(){
+        T temp = peekLeft();
+        size--;
+        list[begin] = null;
+        begin = nextIndex(begin);
+        return temp;
+    }
+
+    public T removeRight(){
+        T temp = peekRight();
+        size--;
+        list[end - 1] = null;
+        end = prevIndex(end);
+        return temp;
+    }
+
+    private int nextIndex(int index) {
+        return (index + 1) % list.length;
+    }
+
+    private int prevIndex(int index) {
+        return (index - 1) >= 0? index - 1 : list.length - 1;
+    }
+
+    private void reCapacity(int newCapacity) {
+        T[] tempArray = (T[]) new Object[newCapacity];
+        System.arraycopy(list, 0, tempArray, 0, size);
+        list = tempArray;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == list.length;
+    }
+
+    /**
+     * @return первый элемент в деке
+     */
+    public T peekLeft() {
+        if (isEmpty()) {
+            return null;
+        }
+        return list[begin];
+    }
+
+    /**
+     * @return последний элемент в деке
+     */
+    public T peekRight() {
+        if (isEmpty()) {
+            return null;
+        }
+        return list[end - 1];
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < list.length; i++) {
+            s += list[i] + " ";
+        }
+        return s;
+    }
+}
--- src/main/java/GeekBrains_Algorithms/Lesson3/main.java
+++ src/main/java/GeekBrains_Algorithms/Lesson3/main.java
@@ -12,16 +12,36 @@ public class main {
 //        Expression exp = new Expression("(1+2)-[3+(x*4}]");
 //        System.out.println(exp.checkBrackets());
 
-        MyQueue<Integer> myQueue = new MyQueue<Integer>(10);
-        for (int i = 0; i < 5; i++) {
-            myQueue.insert(i);
+//        MyQueue<Integer> myQueue = new MyQueue<Integer>(10);
+//        for (int i = 0; i < 5; i++) {
+//            myQueue.insert(i);
+//        }
+//        System.out.println(myQueue);
+//        myQueue.remove();
+//        myQueue.remove();
+//        System.out.println(myQueue);
+//        myQueue.insert(5);
+//        myQueue.insert(6);
+//        System.out.println(myQueue);
+
+        MyDeque<Integer> myDeque = new MyDeque<>(10);
+        for (int i = 1; i < 6; i++) {
+            myDeque.insertRight(i);
         }
-        System.out.println(myQueue);
-        myQueue.remove();
-        myQueue.remove();
-        System.out.println(myQueue);
-        myQueue.insert(5);
-        myQueue.insert(6);
-        System.out.println(myQueue);
+        System.out.println(myDeque);
+        myDeque.insertLeft(6);
+        myDeque.insertLeft(7);
+        System.out.println(myDeque);
+        myDeque.removeLeft();
+        myDeque.removeLeft();
+        myDeque.removeLeft();
+        System.out.println(myDeque);
+        myDeque.removeRight();
+        myDeque.removeRight();
+        myDeque.removeRight();
+        System.out.println(myDeque);
+
+
+
     }
 }
