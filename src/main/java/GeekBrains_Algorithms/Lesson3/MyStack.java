a37c7478d0737d78b254f53f0ac86c59cf9237ef
Natalia Shipova <nataliefrance@mail.ru>
2019-07-21 12:39:26 +0300

    Урок 3. Добавила класс MyStack

---
+++ src/main/java/GeekBrains_Algorithms/Lesson3/MyStack.java
@@ -0,0 +1,62 @@
+package GeekBrains_Algorithms.Lesson3;
+
+public class MyStack<T> {
+
+    private T[] list;
+    private int size;
+    private final int DEFAULT_CAPACITY = 100;
+
+    public MyStack(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("wrong capacity " + capacity);
+        } else {
+            list = (T[]) new Object[capacity];
+            size = 0;
+        }
+    }
+
+    public MyStack() {
+        this.list = (T[]) new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == list.length;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            System.out.println("stack is empty");
+            return null;
+        } else return list[size - 1];
+    }
+
+    public void push(T item) {
+        if (isFull()) {
+            reCapacity(list.length * 2);
+        }
+        list[size] = item;
+        size++;
+    }
+
+    public T pop() {
+        T temp = peek();
+        size--;
+        list[size] = null;
+        return temp;
+    }
+
+    private void reCapacity(int newCapacity) {
+        T[] tempArray = (T[]) new Object[newCapacity];
+        System.arraycopy(list, 0, tempArray, 0, size);
+        list = tempArray;
+    }
+}
---
+++ src/main/java/GeekBrains_Algorithms/Lesson3/main.java
@@ -0,0 +1,15 @@
+package GeekBrains_Algorithms.Lesson3;
+
+public class main {
+    public static void main(String[] args) {
+        MyStack<Integer> myStack = new MyStack<Integer>();
+
+        for (int i = 0; i < 5; i++) {
+            myStack.push(i);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(myStack.pop());
+        }
+    }
+}
