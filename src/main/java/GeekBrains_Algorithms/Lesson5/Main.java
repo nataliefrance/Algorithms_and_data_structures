a3c5f2750a457f6b9a0f8e97fb442a25a3ef7d1a
Natalia Shipova <nataliefrance@mail.ru>
2019-07-29 13:07:19 +0300

    рекурсивные методы

---
+++ src/main/java/GeekBrains_Algorithms/Lesson5/Main.java
@@ -0,0 +1,62 @@
+package GeekBrains_Algorithms.Lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    public static int factorial(int n){
+        if (n <= 1){
+            return 1;
+        }
+        return factorial(n - 1) * n;
+    }
+
+    public static long fibo(int n) {
+        long a = 1;
+        long b = 1;
+        for (int i = 3; i <= n; i++) {
+            b = b + a;
+            a = b - a;
+        }
+        return b;
+    }
+
+    public static long recFibo(long n) {
+        System.out.print(n + " ");
+        if (n < 3) {
+            return 1;
+        }
+        return recFibo(n - 1) + recFibo(n - 2);
+    }
+
+    public static int triangleNum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int recTriangleNum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return recTriangleNum(n - 1) + n;
+    }
+
+    public static int multiply(int a, int b) {
+        int value = 0;
+        for (int i = 0; i < b; i++) {
+            value += a;
+        }
+        return value;
+    }
+
+    public static int recMultiply(int a, int b) {
+        if (b == 1) {
+            return a;
+        }
+        return recMultiply(a, b - 1) + a;
+    }
+}
